e0aa450ac19362623e0cc13e8ea4cdd8b61bc2d9
Alex <toledo_bra@mail.ru>
2023-03-20 21:58:23 +0300

    в работе

--- proba.py
+++ proba.py
@@ -142,5 +142,27 @@
 #     print(last_n, end='')
 
 
+# 12. Дано натуральное число. Напишите программу, которая вычисляет:
+# сумму его цифр;
+# количество цифр в нем;
+# произведение его цифр;
+# среднее арифметическое его цифр;
+# его первую цифру;
+# сумму его первой и последней цифры.
+#
+# n = int(input())
+# total, total_sum = 0, 0
+# total_pro = 1
+# last = n % 10
+# while n != 0:
+#     digit = n % 10       # после цикла буде равно первому числу
+#     total_sum += digit
+#     total += 1
+#     total_pro *= digit
+#     a = total_sum / total
+#     Sum_digit = digit + last
+#     n = n // 10
+# print(total_sum, total, total_pro, a, digit, Sum_digit, sep='\n')
+
 
 
